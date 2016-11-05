a6b2f4f0f431271366cc067a697643965f2380b2
Thomas Duvernay <td75013@hotmail.fr>
2016-11-05 23:46:48 +0100

    main 05/11
    Problème sur les tableaux 2D -> Nécessité de les allouer dynamiquement

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-  float *uxs, *uys, *uzs;
-  float *ux, *uy, *uz;
+struct edks_type {
+    char prefix[128];
+    char date[32];
+    char version[32];
+    char comment[128];
+    unsigned int nlayer;
+    int ndepth;
+    int ndista;
+    float *rho;
+    float *alpha;
+    float *beta;
+    float *thickness;
+    float depthmin;
+    float depthmax;
+    float distamin;
+    float distamax;
+    float *depths;
+    float *distas;
+    float ***zrtdsx;
+};
+
+extern void read_edks_(char *, struct edks_type *);
+extern void read_receivers_(char *, unsigned int *, float *, float *);
+extern void read_patch_(char *, unsigned int *, float *, float *, float *, float *, float *, float *, float *, float *, float *);
+extern void patch2pts_(float *, float *, float *, float *, float *, float *, float *, float *, float *, unsigned int *, unsigned int *, float *, float *, float *, float *);
+extern void mom2disp_(float [][], float [][], float [][], float *, float *, float *, float *, float *, struct edks_type *, float *);
+extern void write_disp_(char *, unsigned int *, unsigned int *, float [][], float [][], float [][]);
+
+int main(int argc, char const *argv[]) {
+  /*float *uxs, *uys, *uzs;
+  float *ux, *uy, *uz;*/
   float *xr, *yr;
   float *xsp, *ysp, *zsp;
   float *x, *y, *z;
@@ -12,12 +39,11 @@ int main(int argc, char const *argv[])
   float *rake, *W, *L, *slip;
   float dw, dy, M;
   float wg, yg, zg;
-  int npw, npy, nrec, np, nspp, irec, i, ip;
+  unsigned int npw, npy, nrec, np, nspp, irec;
   char elem_filename[256], prefix[256];
-  char argum[32];
+  struct edks_type edks;
 
-  if (argc != 7)
-  {
+  if (argc != 7) {
     printf("Usage: sum_layered edks_name geom_prefix #receivers #patches NPW NPY\n");
     exit(0);
   }
@@ -30,6 +56,13 @@ int main(int argc, char const *argv[])
   sscanf(argv[6], "%d", &npy);
 
   nspp = npw * npy; // number of  sources per patch
+
+  float ux[nrec][np] ;
+  float uy[nrec][np] ;
+  float uz[nrec][np] ;
+  float uxs[nrec][nspp] ;
+  float uys[nrec][nspp] ;
+  float uzs[nrec][nspp] ;
 
   strike = (float *) malloc(np * sizeof(float));
   dip = (float *) malloc(np * sizeof(float));
@@ -45,45 +78,37 @@ int main(int argc, char const *argv[])
   xsp = (float *) malloc(nspp * sizeof(float));
   ysp = (float *) malloc(nspp * sizeof(float));
   zsp = (float *) malloc(nspp * sizeof(float));
-  ux = (float *) malloc(np * nrec * sizeof(float));
+  /*ux = (float *) malloc(np * nrec * sizeof(float));
   uy = (float *) malloc(np * nrec * sizeof(float));
   uz = (float *) malloc(np * nrec * sizeof(float));
   uxs = (float *) malloc(nspp * nrec * sizeof(float));
   uys = (float *) malloc(nspp * nrec * sizeof(float));
-  uzs = (float *) malloc(nspp * nrec * sizeof(float));
+  uzs = (float *) malloc(nspp * nrec * sizeof(float));*/
   xr = (float *) malloc(nrec * sizeof(float));
   yr = (float *) malloc(nrec * sizeof(float));
 
-  /*! read in the edks (the greens functions)
-  call read_edks(elem_filename, edks)
+  read_edks_(elem_filename, &edks);
 
-  ! read in the receiver locations
-  call read_receivers(prefix, nrec, xr, yr)
+  read_receivers_(prefix, &nrec, xr, yr);
 
-  ! read in the patch information
-  call read_patch(prefix, np, x, y, z, strike, dip, rake, W, L, slip)
+  read_patch_(prefix, &np, x, y, z, strike, dip, rake, W, L, slip);
 
-  ! need to loop over all patches, get local coordinates and fill single arrays
-  do ip = 1, np
+  for (size_t ip = 0; ip < np; ip++) {
+    patch2pts_(&strike[ip], &dip[ip], &rake[ip], &slip[ip], &W[ip], &L[ip], &x[ip], &y[ip], &z[ip], &npw, &npy, xsp, ysp, zsp, &M);
+    mom2disp_(uxs, uys, uzs, xr, yr, xsp, ysp, zsp, &edks, &M);
 
-     call patch2pts(strike(ip), dip(ip), rake(ip), slip(ip), W(ip), L(ip), &
-                    x(ip), y(ip), z(ip), npw, npy, xsp, ysp, zsp, M)
+    // need to sum pts back to patch contributions where arrays are
+    // indexed as ux(patch,receiver), uxs(pt_source,receiver)
+    for (size_t ii = 0; ii < nrec; ii++) {
+      for (size_t jj = 0; jj < nspp; jj++) {
+        ux[ii][ip] = ux[ii][ip] + uxs[ii][jj];
+        uy[ii][ip] = uy[ii][ip] + uys[ii][jj];
+        uz[ii][ip] = uz[ii][ip] + uys[ii][jj];
+      }
+    }
+  }
 
-     call mom2disp(uxs, uys, uzs, xr, yr, xsp, ysp, zsp, edks, M)
-
-     ! need to sum pts back to patch contributions where arrays are
-     ! indexed as ux(patch,receiver), uxs(pt_source,receiver)
-     ux(ip, :) = sum(uxs, dim=1)
-     uy(ip, :) = sum(uys, dim=1)
-     uz(ip, :) = sum(uzs, dim=1)
-
-  end do
-
-  ! write out displacement arrays
-
-  call write_disp(prefix, np, nrec, ux, uy, uz)
-
-  end program sum_layered*/
+  write_disp_(prefix, &np, &nrec, ux, uy, uz);
 
   return 0;
 }
